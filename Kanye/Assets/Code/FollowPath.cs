b2c185a69d48477c3e4eb2857851f4c8dcabfdbc
unknown <joy.hauser@hotmail.com>
2014-10-19 16:00:00 -0500

    Move Object Along A Line
    These scripts will move any object along a line. This can be used for both
    platforms and enemies for the level we are currently working on.

---
+++ Kanye/Assets/Code/FollowPath.cs
@@ -0,0 +1,73 @@
+﻿// Import libraries
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class FollowPath : MonoBehaviour
+{
+	// Create a type called FollowType
+	public enum FollowType
+	{
+		MoveTowards,
+		Lerp
+	}
+
+	// Create a field that uses the type FollowType and explicitly define this default as 0
+	public FollowType Type = FollowType.MoveTowards;
+	// Define the path that the object needs to follow
+	public PathDefinition Path;
+	// Define a default speed for the object to move at along the line
+	public float Speed = 1;
+	// The maximum distance in the path that is allowed before the object switches to move toward the next point on the path
+	public float MaxDistanceToGoal = .1f;
+
+	// Retrieve path in desired order from PathDefinition script
+	private IEnumerator<Transform> _currentPoint;
+
+	// The method that Unity calls first
+	public void Start()
+	{
+		if (Path == null)
+		{
+			// Print error message and quit
+			Debug.LogError("Path cannot be null", gameObject);
+			return;
+		}
+
+		// Invoke the GetPathEnumerator method and store it in the private variable
+		_currentPoint = Path.GetPathEnumerator();
+
+		// Calls and uses the GetPathEnumerator method
+		_currentPoint.MoveNext();
+
+		// This statement allows you to break out of the start method in the case that there are no points in the path
+		if (_currentPoint.Current == null)
+			return;
+
+		// Set position of the object to the first point in the path
+		transform.position = _currentPoint.Current.position;
+	}
+
+	public void Update()
+	{
+		// This statement will get called if the object was created without a path or at least a path that doesn't have at least one point
+		if (_currentPoint == null || _currentPoint.Current == null)
+			return;
+
+		if (Type == FollowType.MoveTowards)
+			// Move toward the next point in the sequence
+			transform.position = Vector3.MoveTowards (transform.position, _currentPoint.Current.position, Time.deltaTime * Speed);
+		else if (Type == FollowType.Lerp)
+			// Find the linear interpellation of the path between the current point and the next point
+			transform.position = Vector3.Lerp (transform.position, _currentPoint.Current.position, Time.deltaTime * Speed);
+
+		// Determine the distance between the object and the point it is moving toward
+		var distanceSpared = (transform.position - _currentPoint.Current.position).sqrMagnitude;
+
+		// Determine if the object is close enough to the point it is moving toward to start heading to the next point
+		if (distanceSpared < MaxDistanceToGoal * MaxDistanceToGoal)
+			// If it is, start heading to the next point
+			_currentPoint.MoveNext ();
+
+	}
+}
---
+++ Kanye/Assets/Code/PathDefinition.cs
@@ -0,0 +1,56 @@
+﻿// Import libraries
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class PathDefinition : MonoBehaviour
+{
+	// Create point objects to manipulate the points the user creates within this script
+	public Transform[] Points;
+
+	// Move an object along the line
+	public IEnumerator<Transform> GetPathEnumerator()
+	{
+		// If there are not enough points, end the process
+		if (Points == null || Points.Length < 1)
+			yield break;
+
+		// direction = 1 means going to the right/forward
+		var direction = 1;
+		var index = 0;
+
+		while(true)
+		{
+			yield return Points[index];
+
+			// If there is only one point, don't change the direction
+			if (Points.Length == 1)
+				continue;
+
+			// If there are multiple points, go to the end of the line and then reverse direction
+			if (index <= 0)
+				direction = 1;
+			else if (index >= Points.Length - 1)
+				direction = -1;
+
+			// This is where the direction of the object being manipulated gets changed
+			index = index + direction;
+		}
+
+	}
+	
+	public void OnDrawGizmos()
+	{
+		// End the process if there are not enough points to make a line
+		if (Points == null || Points.Length < 2)
+			return;
+
+		// Draw a visual line between the points given by the user
+		for (var i = 1; i < Points.Length; i++)
+		{
+			// Draw a line between the previous point and the current point in that order
+			Gizmos.DrawLine(Points[i - 1].position, Points[i].position);
+		}
+	}
+}
